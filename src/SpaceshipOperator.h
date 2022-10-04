6966facfc800bd5d426ec215a6b3e2e978ca291a
Michael Riegger <mriegger@fastmail.com>
2022-10-03 19:07:06 -0700

    clang format

--- src/SpaceshipOperator.h
+++ src/SpaceshipOperator.h
@@ -1,35 +1,32 @@
 #pragma once
 
-#include <set>
 #include <cassert>
 #include <compare> // needed for the spaceship operator
+#include <set>
 
 // https://www.youtube.com/watch?v=0SeUPb8LC9I
-class SpaceshipOperator
-{
+class SpaceshipOperator {
 public:
-	static void Test()
-	{
-		struct Data
-		{
-			int i;
-			int j;
+  static void Test() {
+    struct Data {
+      int i;
+      int j;
 
-			// Gives us ALL the operators!
-			auto operator<=>(const Data& rhs) const = default;
-		};
+      // Gives us ALL the operators!
+      auto operator<=>(const Data &rhs) const = default;
+    };
 
-		std::set<Data> s;
-		s.emplace(Data{ 5,6 }); // comparisons used by set automatically generated!
+    std::set<Data> s;
+    s.emplace(Data{5, 6}); // comparisons used by set automatically generated!
 
-		Data a{ 1,2 };
-		Data c = a;
-		Data b{ 2,1 };
-		assert(a != b); // automatically generated!
-		assert(a == c); // automatically generated!
+    Data a{1, 2};
+    Data c = a;
+    Data b{2, 1};
+    assert(a != b); // automatically generated!
+    assert(a == c); // automatically generated!
 
-		assert(a < b);
-	}
+    assert(a < b);
+  }
+
 private:
-
 };
