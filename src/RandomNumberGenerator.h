2a8f6916298c5de4ef20654ddc3b04877bfc355c
Michael Riegger <mriegger@fastmail.com>
2022-10-17 23:10:06 -0700

    oop is bad

--- src/RandomNumberGenerator.h
+++ src/RandomNumberGenerator.h
@@ -2,23 +2,21 @@
 
 #include <random>
 
-class RandomNumberGenerator final {
-public:
-  float GetRandomFloat(const float minVal, const float maxVal) {
-    std::uniform_real_distribution<float> uniform_distance(minVal, maxVal);
-    return uniform_distance(m_generator);
-  }
+namespace rng {
+using engine = std::default_random_engine;
 
-  double GetRandomDouble(const double minVal, const double maxVal) {
-    std::uniform_real_distribution<double> uniform_distance(minVal, maxVal);
-    return uniform_distance(m_generator);
-  }
+float randomFloat(engine &e, const float minVal, const float maxVal) {
+  std::uniform_real_distribution<float> uniform_distance(minVal, maxVal);
+  return uniform_distance(e);
+}
 
-  int GetRandomInt(const int minVal, const int maxVal) {
-    std::uniform_int_distribution<int> uniform_distance(minVal, maxVal);
-    return uniform_distance(m_generator);
-  }
+double randomDouble(engine &e, const double minVal, const double maxVal) {
+  std::uniform_real_distribution<double> uniform_distance(minVal, maxVal);
+  return uniform_distance(e);
+}
 
-private:
-  std::default_random_engine m_generator;
-};
+int randomInt(engine &e, const int minVal, const int maxVal) {
+  std::uniform_int_distribution<int> uniform_distance(minVal, maxVal);
+  return uniform_distance(e);
+}
+} // namespace rng
