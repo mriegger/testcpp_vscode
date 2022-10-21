9ab1205a1308d028f1567733cc0ca01ddd42b74b
Michael Riegger <mriegger@fastmail.com>
2022-10-20 20:40:38 -0700

    more EXPECT examples

--- src_tests/main.cpp
+++ src_tests/main.cpp
@@ -7,7 +7,15 @@ struct TestFixtureExample : public ::testing::Test {
   void TearDown() override {}
 };
 
-TEST_F(TestFixtureExample, FixtureExample) {}
+TEST_F(TestFixtureExample, FixtureExample) {
+
+    EXPECT_TRUE(1);
+    EXPECT_EQ(1,1);
+    EXPECT_NE(-1,1);    
+    EXPECT_FLOAT_EQ(1.0001f, 1.0001f);
+    EXPECT_DOUBLE_EQ(1.0001, 1.0001);
+    EXPECT_NEAR(1.0f, 1.001f, 0.01f);
+}
 
 TEST(MKR, StandaloneExample) {}
 
