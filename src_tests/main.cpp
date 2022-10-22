245be4d0b43f05654c3d1ef4b446c1150498d032
Michael Riegger <mriegger@fastmail.com>
2022-10-21 20:52:59 -0700

    google mock working example

--- src_tests/main.cpp
+++ src_tests/main.cpp
@@ -1,5 +1,5 @@
-#include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <gtest/gtest.h>
 
 struct TestFixtureExample : public ::testing::Test {
 
@@ -10,45 +10,41 @@ struct TestFixtureExample : public ::testing::Test {
 
 TEST_F(TestFixtureExample, FixtureExample) {
 
-    EXPECT_TRUE(1);
-    EXPECT_EQ(1,1);
-    EXPECT_NE(-1,1);    
-    EXPECT_FLOAT_EQ(1.0001f, 1.0001f);
-    EXPECT_DOUBLE_EQ(1.0001, 1.0001);
-    EXPECT_NEAR(1.0f, 1.001f, 0.01f);
+  EXPECT_TRUE(1);
+  EXPECT_EQ(1, 1);
+  EXPECT_NE(-1, 1);
+  EXPECT_FLOAT_EQ(1.0001f, 1.0001f);
+  EXPECT_DOUBLE_EQ(1.0001, 1.0001);
+  EXPECT_NEAR(1.0f, 1.001f, 0.01f);
 }
 
 TEST(MKR, StandaloneExample) {}
 
-
-struct Cook
-{
-    virtual ~Cook() = 0;
-    virtual void prepareOrder(const std::string_view order) = 0;
-
+struct Cook {
+  virtual ~Cook() = default;
+  virtual void prepareOrder(const std::string_view) = 0;
 };
 
-struct MockCook : public Cook 
-{
-    MOCK_METHOD(void, prepareOrder, (const std::string_view order), (override));
+// Waiter is the thing we are testing
+struct Waiter final {
+  void update(const std::string_view order, Cook *cook) {
+    cook->prepareOrder(order);
+  }
 };
 
-struct Waiter 
-{
-    void update(std::string_view order, Cook* cook)
-    {
-        cook->prepareOrder(order);
-    }
+struct MockCook : public Cook {
+
+  MOCK_METHOD(void, prepareOrder, (const std::string_view), (override));
 };
 
-TEST(MKR, MockWaiter)
-{
-    using ::testing::AtLeast;                         
-    MockCook mockCook;
-  //  EXPECT_CALL(mockCook, prepareOrder())                  
-//      .Times(AtLeast(1));
-    Waiter w;
-   // w.update();
+TEST(MKR, MockWaiter) {
+  using ::testing::AtLeast;
+  const std::string_view order = "hambaga";
+  MockCook mockCook;
+  EXPECT_CALL(mockCook, prepareOrder(testing::_)).Times(AtLeast(1));
+
+  Waiter w;
+  w.update(order, &mockCook);
 }
 
 int main(int argc, char *argv[]) {
